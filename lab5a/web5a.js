4a261149d3a31947cec44f3bd8524502dabb4182
Romanjjh <mobilonj1258@gmail.com>
2023-11-08 13:00:21 +0200

    save

--- lab5a/web5a.js
+++ lab5a/web5a.js
@@ -11,23 +11,13 @@ const clickLeft = () => {
      }
     }
    
-    // const selectedNodes = document.getElementsByClassName('selected');
-    // const selectedNode = selectedNodes[0];
     const [selectedNode] = document.getElementsByClassName('selected');
-    /**
-     * "class1 class2".toggle('class3') => "class1 class2 class3"
-     * "class1 class2".toggle('class2') => "class1 class3"
-     */
+    
     selectedNode.classList.toggle('selected');
     sliderItemNodes[prevIndex].classList.toggle('selected');
    };
    
-   /**
-    * setTimeout  -> викликає функцію після певного часу 1 раз
-    * setInterval -> викликає функцію кожен раз як інтервал закінчиться
-    */
-   
-   setInterval(clickLeft, 1500); // 1500ms => 1.5s
+
    
    document.getElementById('left')
     .addEventListener('click', clickLeft);
