fb01376374ad629f1010f49d2f9024d641dd18a1
Romanjjh <mobilonj1258@gmail.com>
2023-11-09 20:21:14 +0200

    save

--- lab5b/interface.js
+++ lab5b/interface.js
@@ -1,3 +1,5 @@
+
+
 const Inputa = document.querySelector('.numbera')
 const Inputb = document.querySelector('.numberb')
 const Inputc = document.querySelector('.numberc')
@@ -7,3 +9,45 @@ const result = document.querySelector('.result')
 
 
 Btn.addEventListener('click', heron)
+
+
+
+
+
+//2
+
+const Inputa2 = document.querySelector('.numbera2')
+const Inputb2 = document.querySelector('.numberb2')
+const Inputc2 = document.querySelector('.numberc2')
+const Btn2 = document.querySelector('.btn2')
+const result2 = document.querySelector('.result2')
+
+
+
+Btn2.addEventListener('click', edge)
+
+
+
+
+
+//3
+
+
+
+const Inputa3 = document.querySelector('.numbera3')
+const Inputb3 = document.querySelector('.numberb3')
+const Inputc3 = document.querySelector('.numberc3')
+const Btn3 = document.querySelector('.btn3')
+const result3 = document.querySelector('.result3')
+
+
+
+Btn3.addEventListener('click', hei)
+
+
+
+
+
+
+
+
--- lab5b/lab5b.js
+++ lab5b/lab5b.js
@@ -1,3 +1,6 @@
+
+//1
+
 function heron() {
 
     const a = Number(Inputa.value);
@@ -10,5 +13,54 @@ function heron() {
     console.log(s1);
 
     const answerua = document.querySelector('.result').innerHTML = s1;
+
+}
+
+
+//2
+
+
+function edge() {
+
+    const a = Number(Inputa2.value);
+    const b = Number(Inputb2.value);
+    const c = Number(Inputc2.value);
+
+
+
+   
+
+
+    const s2 = (a*b*c)/2;
+    console.log(s2);
+
+
+    const answerua2 = document.querySelector('.result2').innerHTML = s2;
+
+}
+
+
+
+
+//3
+
+
+
+function hei() {
+
+    const a = Number(Inputa3.value);
+    const b = Number(Inputb3.value);
+   
+
+
+
+   
+
+
+    const s3 = (a*b)/2;
+    console.log(s3);
+
+
+    const answerua3 = document.querySelector('.result3').innerHTML = s3;
 
 }
