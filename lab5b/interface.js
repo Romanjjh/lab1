7393675eec4c8b46cd145a49fe61524004f3a73d
Romanjjh <mobilonj1258@gmail.com>
2023-11-13 19:49:32 +0200

    save

--- lab5b/interface.js
+++ lab5b/interface.js
@@ -1,13 +1,11 @@
 
 
+
 const Inputa = document.querySelector('.numbera')
 const Inputb = document.querySelector('.numberb')
 const Inputc = document.querySelector('.numberc')
 const Btn = document.querySelector('.btn')
 const result = document.querySelector('.result')
-
-
-
 Btn.addEventListener('click', heron)
 
 
@@ -15,14 +13,11 @@ Btn.addEventListener('click', heron)
 
 
 //2
-
 const Inputa2 = document.querySelector('.numbera2')
 const Inputb2 = document.querySelector('.numberb2')
 const Inputc2 = document.querySelector('.numberc2')
 const Btn2 = document.querySelector('.btn2')
 const result2 = document.querySelector('.result2')
-
-
 
 Btn2.addEventListener('click', edge)
 
@@ -31,18 +26,53 @@ Btn2.addEventListener('click', edge)
 
 
 //3
-
-
-
 const Inputa3 = document.querySelector('.numbera3')
 const Inputb3 = document.querySelector('.numberb3')
 const Inputc3 = document.querySelector('.numberc3')
 const Btn3 = document.querySelector('.btn3')
 const result3 = document.querySelector('.result3')
 
-
-
 Btn3.addEventListener('click', hei)
+
+
+
+
+
+//4
+const Inputa4 = document.querySelector('.numbera4')
+const Inputb4 = document.querySelector('.numberb4')
+const Inputc4 = document.querySelector('.numberc4')
+const Inputr4 = document.querySelector('.numberr4')
+const Btn4 = document.querySelector('.btn4')
+const result4 = document.querySelector('.result4')
+
+Btn4.addEventListener('click', radius);
+
+
+
+
+
+//5
+
+
+const Inputa5 = document.querySelector('.numbera5')
+const Btn5 = document.querySelector('.btn5')
+const result5 = document.querySelector('.result5')
+
+Btn5.addEventListener('click', palindrome);
+
+
+
+
+
+//6
+const Inputa6 = document.querySelector('.string1')
+const Inputb6 = document.querySelector('.string2')
+const Btn6 = document.querySelector('.btn6')
+const result6 = document.querySelector('.result6')
+
+Btn6.addEventListener('click', isAnagram);
+
 
 
 
--- lab5b/lab5b.js
+++ lab5b/lab5b.js
@@ -1,6 +1,5 @@
 
 //1
-
 function heron() {
 
     const a = Number(Inputa.value);
@@ -13,39 +12,26 @@ function heron() {
     console.log(s1);
 
     const answerua = document.querySelector('.result').innerHTML = s1;
-
 }
 
 
 //2
-
-
 function edge() {
 
     const a = Number(Inputa2.value);
     const b = Number(Inputb2.value);
     const c = Number(Inputc2.value);
 
-
-
-   
-
-
     const s2 = (a*b*c)/2;
     console.log(s2);
 
-
     const answerua2 = document.querySelector('.result2').innerHTML = s2;
-
 }
 
 
 
 
 //3
-
-
-
 function hei() {
 
     const a = Number(Inputa3.value);
@@ -56,5 +42,120 @@ function hei() {
     console.log(s3);
 
     const answerua3 = document.querySelector('.result3').innerHTML = s3;
-
 }
+
+
+
+
+
+
+//4
+function radius(){
+
+    const a = Number(Inputa4.value);
+    const b = Number(Inputb4.value);
+    const c = Number(Inputc4.value);
+    const r = Number(Inputr4.value);
+
+    const s4 = (a*b*c)/(4*r);
+
+    console.log(s4);
+
+    const answerua4 = document.querySelector('.result4').innerHTML = s4;
+}
+
+
+
+
+
+
+
+//5
+
+function palindrome() {
+    
+    const a = Number(Inputa5.value);
+    
+    const numStr = a.toString();
+
+   
+    const reversedStr = numStr.split('').reverse().join('');
+
+    
+    if (numStr === reversedStr){
+        const answerua5 = document.querySelector('.result5').innerHTML = 'your number is a palindrome'
+    }else{
+        const answerua5 = document.querySelector('.result5').innerHTML = 'your number is not a palindrome'    
+    }
+}
+
+//6
+/**function anagram(){
+    const a = String(Inputa6.value);
+    const b = String(Inputb6.value);
+
+    const isAnagram = isAnagramCheck(a, b);
+
+    const resultElement = document.querySelector('.result6');
+    if (isAnagram) {
+        resultElement.textContent = 'Анаграма!';
+    } else {
+        resultElement.textContent = 'Не анаграма.';
+    }
+
+}**/
+
+
+
+
+function isAnagram(a, b) {
+    // Removes spaces and convert into the lowercase letter
+  
+    a = (Inputa6.value.replace(/\s/g, '').toLowerCase())
+    b = (Inputb6.value.replace(/\s/g, '').toLowerCase())
+    // console.log(str1, str2)
+  
+    if (a.length !== b.length) {
+      return false
+    }
+  
+    // Sort the characters in the given string
+    a = a.split('').sort().join('')
+    b = b.split('').sort().join('')
+  
+    // Compare the sorted String
+  
+    if (a === b){
+        const answerua6 = document.querySelector('.result6').innerHTML = 'your text is a anagrama'
+    }else{
+        const answerua6 = document.querySelector('.result6').innerHTML = 'your text is not a anagrama'
+
+
+
+
+
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
